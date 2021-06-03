964270cc2e30b6b0e81aba9e18a5a598a32b3cfa
Abhinav Kuruvila Joseph <abhinav.kuruvila.joseph@gmail.com>
2021-06-03 15:07:27 +0530

    fix

--- addons/godot_film_maker/recorder.gd
+++ addons/godot_film_maker/recorder.gd
@@ -12,19 +12,25 @@ onready var frames_timer = $FramesTimer
 # Constants
 const REC_DIR = "tmp" # Screenshots will be stored in this directory
 
+
 # Video properties
 var video = {
 	"fps": 24.0,
 	"crf": 60.0,
-	"frames": [],
+	"files": [],
 }
 
 var current_frame = 0
+var zeros = 0
+var effect_idx = 0
 var audio: AudioEffectRecord
+var user_dir = OS.get_user_data_dir()
 
 func start_recording(fps: float,  crf: float):
-	frames_timer.set_wait_time(1/fps)
-	frames_timer.start()
+	frames_timer.start(1/fps)
+	AudioServer.add_bus_effect(0, AudioEffectRecord.new())
+	effect_idx = AudioServer.get_bus_effect_count(0)-1
+	audio = AudioServer.get_bus_effect(effect_idx, 0)
 	audio.set_recording_active(true)
 	create_directory(REC_DIR)
 
@@ -32,11 +38,23 @@ func stop_recording():
 	frames_timer.stop()
 	audio.set_recording_active(false)
 	current_frame = 0
-	audio.get_recording().save_to_wav("user://tmp/audio.waw")
+	audio.get_recording().save_to_wav("user://tmp/audio.wav")
+	video.files.append("audio.wav")
+	AudioServer.remove_bus_effect(0, effect_idx)
+	zeros = len(str(video.files.size()))
+	for i in range(len(video.files)):
+		var new_name = str(i)
+		for j in zeros-len(str(i)):
+			new_name = "0" + new_name
+		new_name = "img" + new_name + ".png"
+		if new_name != video.files[i]:
+			rename_file("/"+REC_DIR+"/"+video.files[i], "/tmp/"+new_name)
+		video.files[i] = new_name
 	_render()
+	remove_directory(REC_DIR, video.files)
 
 func _render():
-	pass # Repace with rendering code
+	pass #Replace with render code
 
 func _ready():
 	init()
@@ -53,7 +71,6 @@ func _on_stop_button_pressed():
 	rec_btn.show()
 	stop_btn.hide()
 	stop_recording()
-	remove_directory(REC_DIR, video.frames)
 
 func _on_pause_button_pressed():
 	pass # Replace with function body.
@@ -68,9 +85,9 @@ func _on_settings_button_pressed():
 
 func _on_Exit_Btn_pressed():
 	video.crf = settings_popup.get_node(
-		"SettingsRow/ValueColumn/CRF_Count").text
+		"SettingsRow/ValueColumn/CRF_Count").value
 	video.fps = settings_popup.get_node(
-		"SettingsRow/ValueColumn/FPS_Count").text
+		"SettingsRow/ValueColumn/FPS_Count").value
 	settings_popup.hide()
 
 func _frame():
@@ -78,7 +95,7 @@ func _frame():
 	var frame = get_tree().get_root().get_texture().get_data()
 	frame.flip_y()
 	frame.save_png("user://"+REC_DIR+"/img"+str(current_frame)+".png")
-	video.frames.append("img"+str(current_frame)+".png")
+	video.files.append("img"+str(current_frame)+".png")
 	current_frame += 1
 
 
@@ -94,3 +111,13 @@ func remove_directory(dir_name: String, contents:Array):
 	for i in contents:
 		dir.remove(dir_name+"/"+i)
 	dir.remove(dir_name)
+
+func rename_file(from: String, to: String):
+	var output
+	match OS.get_name():
+		"X11":
+			output = OS.execute("mv", [user_dir+from, user_dir+to], true)
+		"OSX":
+			output = OS.execute("mv", [user_dir+from, user_dir+to], true)
+		"windows":
+			output = OS.execute("rename", [user_dir+from, user_dir+to], true)
